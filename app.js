7d6138370aca751440db5d8eeed6861dfc14c71f
Mason Banks <masonbanks@ymail.com>
2018-08-17 17:46:35 +0100

    app.js added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const apiRouter = require('./routes/api');
-const { DB_URL } = require('./config/index')
+const { DB_URL } = process.env || require('./config/index')
 const body_parser = require('body-parser')
 
 app.use(body_parser.json())
